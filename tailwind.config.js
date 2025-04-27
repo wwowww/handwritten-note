c19c256e47348c15755054ee016d5540861a47e6
wwowww <105021733+wwowww@users.noreply.github.com>
2025-04-28 01:42:52 +0900

    design. max height theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       boxShadow: {
         'custom': 'rgba(0,0,0,.30) 0 2px 3px',
       },
+      maxHeight: {
+        'minus-134': 'calc(100% - 134px)',
+      }
     },
   },
   plugins: [],
