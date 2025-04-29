ee75bcf735fa36cfde0f1f272775e998b2b26fdc
wwowww <105021733+wwowww@users.noreply.github.com>
2025-04-29 22:58:32 +0900

    refactor. 이전, 다음 버튼 비활성화 및 호버 처리

--- src/components/PageControls.tsx
+++ src/components/PageControls.tsx
@@ -3,18 +3,41 @@ import { usePdfStore } from '@/stores/usePdfStore';
 const PageControls = () => {
   const { pageNumber, totalPages, prevPage, nextPage } = usePdfStore();
 
+  const isPrevDisabled = pageNumber <= 1;
+  const isNextDisabled = pageNumber >= totalPages;
+
   return (
     totalPages > 1 &&
     <div className="absolute left-[50%] t-0 translate-x-[-50%] flex items-center gap-2">
-      <button onClick={prevPage} disabled={pageNumber <= 1} className="text-sm bg-gray-100 py-1 px-2 cursor-pointer rounded-md text-gray-700">
+      <button
+        onClick={prevPage}
+        disabled={isPrevDisabled}
+        className={`
+          text-sm bg-gray-100 py-1 px-2 rounded-md text-gray-700
+          ${isPrevDisabled
+            ? 'opacity-70 cursor-not-allowed'
+            : 'cursor-pointer hover:bg-gray-300'
+          }
+        `}
+      >
         이전
       </button>
       <span className='text-sm'>{pageNumber} / {totalPages}</span>
-      <button onClick={nextPage} disabled={pageNumber >= totalPages} className="text-sm bg-gray-100 py-1 px-2 cursor-pointer rounded-md text-gray-700">
+      <button
+        onClick={nextPage}
+        disabled={isNextDisabled}
+        className={`
+          text-sm bg-gray-100 py-1 px-2 rounded-md text-gray-700
+          ${isNextDisabled
+            ? 'opacity-70 cursor-not-allowed'
+            : 'cursor-pointer hover:bg-gray-300'
+          }
+        `}
+      >
         다음
       </button>
     </div>
   );
 }
 
-export default PageControls
+export default PageControls;
