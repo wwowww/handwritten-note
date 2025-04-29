9e84ab646369d12ac7bf81400877d8d4b18bab45
wwowww <105021733+wwowww@users.noreply.github.com>
2025-04-29 22:57:38 +0900

    feat. 새파일 업로드 시, 이전 필기 삭제 기능 추가

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,8 +1,10 @@
 import { useRef } from 'react';
 import { useNoteStore } from '@/stores/useNoteStore';
+import { useDrawingStore } from '@/stores/useDrawingStore';
 
 const FileUploader = () => {
   const { setFile } = useNoteStore();
+  const { clearPage } = useDrawingStore();
   const fileInputRef = useRef<HTMLInputElement>(null);
   const inputId = 'file-uploader-input';
 
@@ -10,6 +12,7 @@ const FileUploader = () => {
     const file = e.target.files?.[0];
     if (file && file.type === 'application/pdf') {
       setFile(file);
+      clearPage();
     } else if (file) {
       alert('PDF 파일만 업로드할 수 있습니다.');
       if (fileInputRef.current) {
